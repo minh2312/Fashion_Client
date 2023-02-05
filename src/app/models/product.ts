119a2387d3958da5afc993ffb4a14dfdf823850d
Minh Lee <minhaz23122002@gmail.com>
2023-02-05 16:32:58 +0700

    update

---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+    id?: number;
+    idCategory?: number;
+    codeProduct?: string;
+    nameProduct?: string;
+    price?: any;
+    salePrice?: any;
+    image?: string;
+    description?: string;
+    status?: any;
+    deleteFlag?: number;
+    createAt?: Date;
+    updateAt?: Date;
+}
